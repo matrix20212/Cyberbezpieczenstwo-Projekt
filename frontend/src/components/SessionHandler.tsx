bd37fc49ed17418ed7612c267f1cbb2c3ff7d4c6
matrix20212 <mateuszrudnik40@gmail.com>
2025-10-24 16:42:52 +0200

    update time

--- frontend/src/components/SessionHandler.tsx
+++ frontend/src/components/SessionHandler.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const SESSION_TIMEOUT_SECONDS = 1 * 60; //5 minut
+const SESSION_TIMEOUT_SECONDS = 5 * 60; //5 minut
 
 export default function SessionHandler({ children }: { children: React.ReactNode }) {
   const [timeLeft, setTimeLeft] = useState(SESSION_TIMEOUT_SECONDS);
